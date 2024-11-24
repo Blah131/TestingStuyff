c412d74eee052734b3e83bca344d43e27e9bcde0
Kane <kbukowski@ivytech.edu>
2024-11-24 12:07:46 -0600

    Removed semi colons in index.js up top

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 app.use(bodyParser.json());
